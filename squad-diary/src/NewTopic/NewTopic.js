8c00d6e26bd57229d529d57cde1bc3bc68b408bf
Noon <noon@Noons-MacBook-Pro.local>
2018-07-11 15:10:43 -0400

    finish adding new entry and new topic

--- squad-diary/src/NewTopic/NewTopic.js
+++ squad-diary/src/NewTopic/NewTopic.js
@@ -7,7 +7,7 @@ class NewTopic extends Component {
         this.state = {
             newLunchTopic: '',
             newEntry: '',
-            WeekNumber:''
+            weekNumber: null
         }
 
         if (window.location.origin === "http://localhost:3000") {
@@ -18,6 +18,8 @@ class NewTopic extends Component {
         
         this.handleChange = this.handleChange.bind(this)
         this.handleSubmit = this.handleSubmit.bind(this)
+        this.handleNewEntry = this.handleNewEntry.bind(this)
+        this.handleNewTopic = this.handleNewTopic.bind(this)
     }
 
     handleChange (e ) {
@@ -25,8 +27,7 @@ class NewTopic extends Component {
         
     }
 
-    handleSubmit (e) {
-        e.preventDefault()
+    handleNewTopic (){
         console.log(this.state)
         const newLunchTopic = {
             content: this.state.newLunchTopic
@@ -39,19 +40,38 @@ class NewTopic extends Component {
         .catch(err => console.log(err))
         // console.log(newLunchTopic)
     }
+
+    handleNewEntry (){
+        console.log(this.state.weekNumber)
+        axios.post(`${this.origin}/entry`, {weekNumber: this.state.weekNumber,
+        content: this.state.newEntry})
+        .then(res => {
+            console.log(`in handleNewEntry`)
+            console.log(res)
+            console.log(res.data)
+        })
+    }
+
+    handleSubmit (e) {
+        e.preventDefault()
+       this.handleNewTopic()
+       this.handleNewEntry()
+
+       
+    }
     render() {
 
         return (
             <div>
-                <div className="addLunchTopic">
+                <div className="addTopic">
                     <h3>Add New Lunch Topic</h3>
                     <form onSubmit={this.handleSubmit}>
                         <label>Suggest Lunch Topic: </label>
                         <textarea  name="newLunchTopic" value={this.state.value}  onChange={this.handleChange} /> <br />
-                        <label>Content</label>
-                        <textarea name="newEntry" value={this.state.value} onChange={this.handleChange} placeholder="Please put some recommendation, high, or low "/> <br />
+                        <label>Share Your experience:</label>
+                        <textarea name="newEntry" value={this.state.value} onChange={this.handleChange} placeholder="Your recommendation, high, or low "/> <br />
                         <label>WeekNumber: </label>
-                        <input type="number" name="weekNumber" /> <br />
+                        <input type="number" name="weekNumber" value={this.state.value} onChange={this.handleChange} /> <br />
                         <button> Add Topic</button>
                     </form>
                 </div>
