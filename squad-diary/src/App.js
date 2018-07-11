f9f47793a89a193c1771a6681d011ccc5ad9f977
Mariya <mariyawhitcomb@gmail.com>
2018-07-11 09:25:59 -0400

    added axios get request

--- squad-diary/src/App.js
+++ squad-diary/src/App.js
@@ -1,7 +1,35 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import axios from "axios";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      entries: []
+    };
+
+    if (window.location.origin === "http://localhost:3000") {
+      this.origin = "http://localhost:4000";
+    } else {
+      this.origin = "https://cryptic-meadow-80214.herokuapp.com";
+    }
+  }
+  getEntries = () => {
+    axios
+      .get(`${this.origin}`)
+      .then(results => {
+        this.setState({ items: results.data });
+        console.log(results.data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+  componentDidMount() {
+    this.getEntries();
+  }
+
   render() {
     return (
       <div className="App">
