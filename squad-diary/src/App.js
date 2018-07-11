574aed863bae8a450cf6edc73b9277cb35f53e18
Malik Keenen Ahmed <kansomalik@gmail.com>
2018-07-11 09:40:12 -0400

    added links routes and switch

--- squad-diary/src/App.js
+++ squad-diary/src/App.js
@@ -1,14 +1,56 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from 'react'
+import {Route, Link, Switch} from 'react-router-dom'
+import LunchTopic from './LunchTopic/LunchTopic'
+import ShowAdmin from './ShowAdmin/ShowAdmin'
+import ShowListAll from './ShowListAll/ShowListAll'
+import ShowListWeek from './ShowListWeek/ShowListWeek'
+import ShowRandom from './ShowRandom/ShowRandom'
+import './App.css'
 
 class App extends Component {
-  render() {
+  render () {
     return (
-      <div className="App">
-        <h1>TITLE PAGE</h1>
+      <div className='App'>
+        <header>
+          <h1>TITLE PAGE</h1>
+          <nav>
+            <Link to='/'>
+          Random
+            </Link>
+            <Link to='/list'>
+          All
+            </Link>
+            <Link to='/week'>
+          By week
+            </Link>
+            <Link to='/lunch'>
+          Lunch Topics
+            </Link>
+          </nav>
+        </header>
+        <main>
+          <Switch>
+            <Route
+              path='/'
+              component={ShowRandom}
+            />
+            <Route
+              path='/list'
+              component={ShowListAll}
+            />
+            <Route
+              path='/week'
+              component={ShowListWeek}
+            />
+            <Route
+              path='/lunch'
+              component={LunchTopic}
+            />
+          </Switch>
+        </main>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
