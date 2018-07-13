efc1812f74439c760986cb6ae18f0548c2cc0b9c
Malik Keenen Ahmed <kansomalik@gmail.com>
2018-07-13 11:31:56 -0400

    made readme.md

--- squad-diary/src/ShowRandom/ShowRandom.js
+++ squad-diary/src/ShowRandom/ShowRandom.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import './ShowRandom.css'
 
 class ShowRandom extends Component {
   componentDidMount() {
@@ -14,8 +15,8 @@ class ShowRandom extends Component {
           <div className="col-lg-2 col-md-2" />
           <div className="col-lg-8 col-md-8 col-sm-12">
             <div className="card">
-              <div className="card-header">Welcome to the Squad Diaries Homepage
-                <p className="card-text">
+              <div className="card-body">
+                <p className="card-text">Welcome to the Squad Diaries Homepage <br /><br />
                   This is a space where students can anonymously share how they're feeling during their cohort.<br /><br /> Go ahead and click on the add new entry link and head to the form to share your experience. <br />
                   Also you can share interesting topics you've discussed with your classmates during lunch. <br /><br />If you head over to the main list of submissions, you'll able to click on the "week #" link to see all of the posts for that specific week in the cohort.
                   <br /><br />
