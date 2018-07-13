4390f91aa8dd94401b2f49231c1230e2fecd457d
Malik Keenen Ahmed <kansomalik@gmail.com>
2018-07-13 09:18:54 -0400

    hp styling going to pull from the others now

--- squad-diary/src/ShowRandom/ShowRandom.js
+++ squad-diary/src/ShowRandom/ShowRandom.js
@@ -13,6 +13,16 @@ class ShowRandom extends Component {
         <div className="row">
           <div className="col-lg-2 col-md-2" />
           <div className="col-lg-8 col-md-8 col-sm-12">
+            <div className="card">
+              <div className="card-header">Welcome to the Squad Diaries Homepage
+                <p className="card-text">
+                  This is a space where students can anonymously share how they're feeling during their cohort.<br /><br /> Go ahead and click on the add new entry link and head to the form to share your experience. <br />
+                  Also you can share interesting topics you've discussed with your classmates during lunch. <br /><br />If you head over to the main list of submissions, you'll able to click on the "week #" link to see all of the posts for that specific week in the cohort.
+                  <br /><br />
+
+                </p>
+              </div>
+            </div>
             <blockquote class="quote-card">
               <p>{random && random.content}</p>
             </blockquote>
