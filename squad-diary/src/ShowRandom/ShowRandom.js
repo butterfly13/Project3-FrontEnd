aacb7882d0ef1733a5883af1db80ba57af53f72b
Mariya <mariyawhitcomb@gmail.com>
2018-07-13 10:30:06 -0400

    working on welcome page

--- squad-diary/src/ShowRandom/ShowRandom.js
+++ squad-diary/src/ShowRandom/ShowRandom.js
@@ -12,21 +12,27 @@ class ShowRandom extends Component {
       <div className="container">
         <div className="row">
           <div className="col-lg-2 col-md-2" />
-          <div className="col-lg-8 col-md-8 col-sm-12">
-            <div className="card">
-              <div className="card-header">Welcome to the Squad Diaries Homepage
-                <p className="card-text">
-                  This is a space where students can anonymously share how they're feeling during their cohort.<br /><br /> Go ahead and click on the add new entry link and head to the form to share your experience. <br />
-                  Also you can share interesting topics you've discussed with your classmates during lunch. <br /><br />If you head over to the main list of submissions, you'll able to click on the "week #" link to see all of the posts for that specific week in the cohort.
-                  <br /><br />
-
-                </p>
-              </div>
+          <div className="card">
+            <div className="card-header">
+              Welcome to the Squad Diary
+              <p className="card-text">
+                This is a space where you can find encouragement and motivation,
+                share how you feel at any point during your cohort and realize
+                you are not alone.<br />
+                Go ahead and share your experience. The good, the bad and the
+                ugly! <br />
+                If you have an amazing lunch conversation please share! We would
+                love to know!<br />
+                If you have a particular week in mind head over and see how
+                different/simmilar is your experience!<br />
+                We are so glad you are here! Welcome to GA family!
+                <br />
+              </p>
             </div>
-            <blockquote class="quote-card">
-              <p>{random && random.content}</p>
-            </blockquote>
           </div>
+          <blockquote class="quote-card">
+            <p>{random && random.content}</p>
+          </blockquote>
         </div>
       </div>
     );
