cefd53efd9875a77b39d9a497b6699c89fbb453a
Mariya <mariyawhitcomb@gmail.com>
2018-07-13 11:38:06 -0400

    fixed admin style

--- squad-diary/src/ShowAdmin/ShowAdmin.js
+++ squad-diary/src/ShowAdmin/ShowAdmin.js
@@ -16,9 +16,9 @@ class ShowAdmin extends Component {
           <div className="col-lg-8 col-md-8 col-sm-12">
             <div className="container">
               <blockquote className="quote-card">
-                <p>Are you sure you are THE admin?</p>
+                <h4>Are you sure you are THE admin?</h4>
                 <button
-                  className="btn btn-lg btn-block btn-secondary"
+                  className="btn btn-lg btn-block btn-secondary text"
                   onClick={this.onClick}
                   type="submit"
                 >
